e6565e62f5715c23b2d71d6372f9d87dc9a7db85
erifranck <erifranjose@hotmail.com>
2019-04-14 16:25:28 -0500

    add color theme and font family

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import {render} from 'react-dom';
-import {createGlobalStyles} from 'styled-components';
 import { createGlobalStyle } from 'styled-components'
 import {App} from "layouts/App/App";
 import {Fragment} from "react";
+import {theme} from "theme";
 
 const GlobalStyle = createGlobalStyle`
+    html {
+        font-family: ${theme.fontFamily};
+    }
     * {
         margin: 0px;
         padding: 0px;
