03db2f17ce753baf07869c360686bd381a1ed098
erifranck <erifranjose@hotmail.com>
2019-04-14 16:25:46 -0500

    add new components

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -4,41 +4,23 @@ import {Provider} from "react-redux"
 import {ThemeProvider, theme} from 'theme'
 import {Header} from "components/Header/Header";
 import {Footer} from "components/Footer/Footer";
-import {Exchanger} from "components/Exchanger/Exchanger";
 import {createStore} from "store/createStore";
-import {actions} from "store/reducers/exchange";
+import {Stories} from "components/Stories/Stories";
+import {Tips} from "components/Tips/Tips";
+import {Resources} from "components/Resources/Resources";
 
 const store = createStore();
 export class App extends React.Component {
-    static state: {readonly eur: string} = {
-        eur: ''
-    }
-    constructor(props) {
-        super(props);
-        this.state = {
-            eur: ''
-        }
-    }
-    componentDidMount() {
-        store.dispatch(actions.getExchangeValue());
-        store.subscribe(() => {
-            this.setState({
-                eur: store.getState().exchange.eur
-            })
-        })
-    }
-    onCalculate(usd: string) {
-        store.dispatch(actions.exchange(Number(usd)));
-    }
     render () {
-        console.log(this.state);
         return (
             <Provider store={store}>
                 <ThemeProvider theme={theme}>
                     <Fragment>
                         <Header />
-                        <Exchanger onCalculate={this.onCalculate} eur={this.state['eur']}/>
-                        <Footer />
+                        <Stories />
+                        <Tips />
+                        <Resources />
+                        <Footer/>
                     </Fragment>
                 </ThemeProvider>
             </Provider>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -11,6 +11,11 @@ const {
 export interface IThemeInterface {
     primaryColor: string;
     secondaryColor: string;
+    white: string;
+    grey: string;
+    blue: string;
+    yellow: string;
+    orange: string;
     fontSmall: string;
     fontMedium: string;
     fontLarge: string;
@@ -20,16 +25,21 @@ export interface IThemeInterface {
 }
 
 export const theme = {
-    primaryColor: "#1ca2bb",
-    secondaryColor: "#ffdd93",
-    grey: "#cfcfcf",
+    primaryColor: "#6473AA",
+    secondaryColor: "#BE6638",
+    white: '#fff',
+    grey: "#9F9EB9",
+    blue: "#6473AA",
+    yellow: "#D49D4B",
+    orange: "#BE6638",
     depth: '0 1px 8px 0 rgba(0, 0, 0, 0.01)',
     depth2: '0 1px 8px 0 rgba(0, 0, 0, 0.1)',
     fontSmall: "12px",
     fontMedium: "14px",
     fontLarge: "16px",
-    fontTitle: "18px",
-    fontFamily: "San Serif",
+    fontSubTitle: "24px",
+    fontTitle: "36px",
+    fontFamily: "'Muli', sans-serif",
     borderRadius: "5px",
 };
 
